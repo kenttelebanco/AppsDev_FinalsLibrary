7793b95bf410ac3003e01ddd47614bac16492345
Gilben <gilbenferolinoz@gmail.com>
2022-06-08 09:21:53 +0800

    Added book admin functionality

---
+++ src/app/models/books/book.ts
@@ -0,0 +1,14 @@
+
+enum Book_Status{
+    BRW = "BORROWED",
+    AVL = "AVAILABLE",
+    DUE = "DUE",
+    RMV = "REMOVED",
+}
+
+export interface Book {
+    id?: string;
+    name?: string;
+    status?: Book_Status;
+    author: string;
+}
