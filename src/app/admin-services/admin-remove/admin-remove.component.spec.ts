24ab6b4c000eba9d47a6b0722f9b3522ccff1744
Kent Telebanco <ktelebanco@gmail.com>
2022-06-14 15:58:42 +0800

    Borrow and Return Conditions, Modified Lines

--- src/app/admin-services/admin-remove/admin-remove.component.spec.ts
+++ src/app/admin-services/admin-remove/admin-remove.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 import { AdminRemoveComponent } from './admin-remove.component';
 
@@ -8,6 +14,8 @@ describe('AdminRemoveComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ ReactiveFormsModule,FormsModule, AngularFireModule.initializeApp(environment.firebase), provideFirestore(() => getFirestore()),
+        provideFirebaseApp(() =>initializeApp(environment.firebase)),provideAuth(()=>getAuth())], 
       declarations: [ AdminRemoveComponent ]
     })
     .compileComponents();
